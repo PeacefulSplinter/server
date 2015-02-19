a808d39caee9c0f62164a2455361bdd3d52d8ff7
Steven Williams <Steven30135@gmail.com>
2015-02-18 21:16:06 -0800

    deploy test

--- api/user/local/index.js
+++ api/user/local/index.js
@@ -3,7 +3,7 @@ var controller = require('./userController.js');
 
 var router = express.Router();
 
-router.get('/local/login', controller.login);
+router.post('/local/login', controller.login);
 router.post('/local/register', controller.register);
 
 module.exports = router;
