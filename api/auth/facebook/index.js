df7fb1ca721311f02b8439a896498337d45f650b
Steven Williams <Steven30135@gmail.com>
2015-02-19 19:37:54 -0800

    push to deployment

--- api/auth/facebook/index.js
+++ api/auth/facebook/index.js
@@ -7,11 +7,21 @@ router.get('/facebook', passport.authenticate('facebook'), function (req, res){
 });
 
 router.get('/facebook/callback', function(req, res, next){
+	console.log('test')
+	res.send('we in this!');
 	passport.authenticate('facebook', function(err, user, info){
 		if(err) {return next(err)}
-		console.log('test!');
-		res.send('we in this!');
 	})(req, res, next);
 });
 
 module.exports = router;
+
+// , function (req, res) {
+// 	res.status(200).send({msg: 'Yoloswag360360quickscopedropshotheadshot'});
+  // res.sendFile(__dirname + '../../../test.html', function(err){
+  //   if(err) {
+  //     res.status(err.status).end();
+  //   }
+  //   var token = jwt.sign({foo:'foobar'}, $config.JWT_SECRET, {expiresInMinutes: 60*5});
+  //   res.status(200).json({token: token}).end();
+  // });
